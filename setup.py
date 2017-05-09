81e900bd36deca133ced68088863a3a47d8825d7
彭伟 <1300896173@qq.com>
2017-05-09 15:27:13 +0800

    添加setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='flask_backstage',
+      version='0.1',
+      description='A backstage framework with very few code.',
+      author='pengwei',
+      author_email='1300896173@qq.com',
+      url='https://github.com/huanglong123/flask-backstage',
+      packages=['flask_backstage'],
+      license='MIT',
+      platforms='any',
+      install_requires=[
+          'Flask>=0.7',
+          'Flask-WTF',
+          'Flask-Bcrypt',
+          'Flask-Login',
+          'flask-mongoengine',
+          'Flask-WTF'
+      ],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Programming Language :: Python :: 3.5',
+                   ],
+      )
