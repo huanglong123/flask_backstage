8fe3881549b1f88bfbf417f7b09fac49ec846597
彭伟 <1300896173@qq.com>
2018-06-29 16:59:52 +0800

    作者邮箱更改

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='flask_backstage',
-      version='0.2.1',
+      version='0.2.2',
       description='A backstage framework with very few code.',
       author='pengwei',
       author_email='huanglongtiankong123@gmail.com',
